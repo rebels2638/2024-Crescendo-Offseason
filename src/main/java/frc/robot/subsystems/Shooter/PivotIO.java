582ecd4d82dbcbdb7ce39940e6cb24fe71ab5201
Edan Liahovetsky <edan.liahovetsky@icloud.com>
2024-05-14 19:29:37 -0400

    initial commit

---
+++ src/main/java/frc/robot/subsystems/Shooter/PivotIO.java
@@ -0,0 +1,12 @@
+import org.littletonrobotics.junction.AutoLog;
+
+public interface PivotIO {
+    @AutoLog
+    public static class PivotIOInputs {
+        public double currentAmps;
+        public doub
+    }
+
+    public default void setVoltage(double voltage) {}
+    
+}
